777c09d4747ab72e3b43950e78853dcee204b2fa
dipperlost <dipper>
2022-08-18 18:33:13 +0300

    task123

---
+++ task23/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите любое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if(N == 0)
+{
+    Console.WriteLine("0");
+}
+else
+{
+while(count <= N)
+{
+    Console.WriteLine(Math.Pow(count, 3));
+    count++;
+}
+}
